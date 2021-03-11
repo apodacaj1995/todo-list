8e5eb874730e197d6e8fbd50ad481a898130f5f5
apodacaj1995 <jaclynkapodaca@gmail.com>
2021-03-10 19:09:23 -0700

    first commit

---
+++ To-Do-List-master/js/app.js
@@ -0,0 +1,63 @@
+// CODE EXPLAINED channel
+
+// select the elements
+
+const clear = document.querySelector("clear");
+const dateElement = document.getElementById ("date");
+const list = document.getElementById ("list");
+const input = document.getElementById ("input");
+
+// classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST, id;
+
+// Show today's date
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// add to do function
+function addToDo(toDo, id, done, trash){
+    if(trash){ return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `
+    <li class="item">                    
+        <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+        <p class="text ${LINE}">${toDo}</p>
+        <i class="fa fa-trash-o de" job="delete" id=${id}></i>
+    </li>
+`;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+
+// add an item to the list using the enter key
+document.addEventListener("keyup",function(even){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+
+    // if the input isn't empty
+    if(toDo){
+        addToDo(toDo);
+
+        LIST.push({
+            name : toDo,
+            id : id,
+            done : false,
+            trash : false
+        });
+    }
+    input.value = "";
+
+    }
+});
+
+addToDo("Coffee", 1, false, false);
