d3b36de35e35d075aa058d8b84f8bcc1226766da
apodacaj1995 <jaclynkapodaca@gmail.com>
2021-03-10 20:12:51 -0700

    finished to-do list

--- To-Do-List-master/js/app.js
+++ To-Do-List-master/js/app.js
@@ -21,6 +21,8 @@ const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
+
+
 // add to do function
 function addToDo(toDo, id, done, trash){
     if(trash){ return; }
@@ -46,7 +48,7 @@ document.addEventListener("keyup",function(even){
 
     // if the input isn't empty
     if(toDo){
-        addToDo(toDo);
+        addToDo(toDo, id, false, false);
 
         LIST.push({
             name : toDo,
@@ -60,4 +62,67 @@ document.addEventListener("keyup",function(even){
     }
 });
 
-addToDo("Coffee", 1, false, false);
+    // add item to localstorage (this code must be added where teh LIST array is updated)
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+
+// complete to do
+
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH)
+
+    LIST[element.id].done = LIST[element.id].don ? false : true;
+}
+
+// remove to do
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+// target the items created dynamically
+
+list.addEventListener("click", function(event){
+    const element = event. target; // retun the clicked element inside list
+    const elementJob = element.attributes.job.value;  // complete or delete
+
+    if(elementJob == "complete"){
+        completeToDo(element);    
+    }else if (elementJob == "delete"){
+        removeToDo(element);
+    }
+
+    // add item to localstorage (this code must be added where teh LIST array is updated)
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+
+})
+
+// // get item from localstorage
+let date = localStorage.getItem("TODO");
+
+// check if data is not empty
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+}else{
+    // if data isn't empty
+    LIST = [];
+    id = 0;
+}
+
+// load items to the user's interface
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+// to clear the local storage
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
